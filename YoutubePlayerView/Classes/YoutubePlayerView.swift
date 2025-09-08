ebd2b59c72ce19bdaf575bab5998ad74d56e8f01
Dino <40366083+pokeduck@users.noreply.github.com>
2025-09-08 17:35:38 +0800

    Update YoutubePlayerView.swift
    - 加上 <meta name="referrer" content="strict-origin-when-cross-origin">
      以強制 WKWebView 使用 strict-origin-when-cross-origin referrer policy

--- YoutubePlayerView/Classes/YoutubePlayerView.swift
+++ YoutubePlayerView/Classes/YoutubePlayerView.swift
@@ -460,6 +460,7 @@ extension YoutubePlayerView {
       padding: 0;
     }
   </style>
+  <meta name="referrer" content="strict-origin-when-cross-origin">
   <meta
     name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no"
